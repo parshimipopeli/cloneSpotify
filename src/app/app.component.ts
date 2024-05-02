204efbdcaae5da2c693b3d90ae5ba505eace5b1e
Philippe Lamorski <philippe.lamorskipro@gmail.com>
2024-05-02 11:19:09 +0200

    Ajout du composant library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavigationComponent } from "./layout/navigation/navigation.component";
+import { LibraryComponent } from "./layout/library/library.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, FontAwesomeModule, NavigationComponent, LibraryComponent]
 })
 export class AppComponent implements OnInit {
   title = 'front';
